1b8a21489d59b6ef8ea697d4c1c5b7f0b5b22bea
pratyush-who <pratyushmehra2005@gmail.com>
2026-05-29 02:35:04 +0530

    kafka

--- consumer/src/main/java/com/example/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/example/consumer/KafkaConsumer.java
@@ -7,7 +7,13 @@ import org.springframework.stereotype.Component;
 public class KafkaConsumer {
 
     @KafkaListener(topics = "topic", groupId = "my-group")
-    public void listen(String message){
-        System.out.println(message);
+    public void listen1(String message){
+        System.out.println("ef"+message);
+    }
+
+    @KafkaListener(topics = "topic", groupId = "my-group-2")
+    //grp id diff hogi to 2 consumer same hui to ek hi mana jaega
+    public void listen2(String message){
+        System.out.println("efdsds"+message);
     }
 }
